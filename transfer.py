fd95a9cc57cadc946fb3a9bd5e5d7cdaf8a8e961
Valink16 <valuthyk@gmail.com>
2018-04-02 23:45:47 +0200

    Little test

--- transfer.py
+++ transfer.py
@@ -1,13 +1,13 @@
 import myLib, time, socket, sys, os
 from threading import Thread
 
+MB100 = 1024 * 1024 * 100
+
 def send():
 	fichier = b''
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	nomFichier = input("Enter file name: ")
-
 	with open(nomFichier, "rb") as file:
-		myLib.log("{} Open on rb".format(nomFichier))
 		fichier = file.read()
 	port = int(input("OPEN PORT: "))
 	server.bind(('', port))
@@ -15,13 +15,15 @@ def send():
 	myLib.log("Listening")
 	client, infos = server.accept()
 	myLib.log("Someone just connected himself: IP: {}, PORT: {}".format(infos[0], infos[1]))
-	myLib.log("Length of opened file :{} bytes".format(sys.getsizeof(fichier)))
-	length = sys.getsizeof(fichier)
+	length = os.path.getsize(nomFichier)
+
+
 	client.send((str(length) + ',' + nomFichier).encode())
 	myLib.log("Sleeping 1s to be sure client is ready")
 	time.sleep(1)
 
 	myLib.log("Sending...")
+	myLib.log("{} Open on rb".format(nomFichier))
 	temps = time.time()
 	client.send(fichier)
 	temps = time.time() - temps
@@ -35,10 +37,10 @@ def receive(recvBufferSize = 1000000, saveBufferSize = 150):
 	client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	ip = input("Enter sender's IP: ")
 	port = int(input("Enter sender's open PORT: "))
-	
+
 	client.connect((ip,port))
 	myLib.log('Connected')
-	
+
 	recu = client.recv(1024).decode("utf-8") # Sender immediately sends a message when connected
 	taille, fileName = recu.split(',')#recu contains the length of file and file extension, separated by a comma
 	taille = int(taille)
@@ -70,7 +72,7 @@ def receive(recvBufferSize = 1000000, saveBufferSize = 150):
 
 	myLib.log('{}/{} in {}s'.format(tailleRecu,taille,duree))
 	myLib.log("All received !")
-	
+
 	myLib.log('Average speed: {} MB/s'.format(os.path.getsize(fileName)/1000000/duree))
 
 	myLib.log("Bye !")
